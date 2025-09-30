13bc68b0d9a7349626c644c706aca12f0c4e00aa
EASports98 <evanmalott3715@gmail.com>
2025-09-30 10:47:31 -0400

    9/30/2025
    Finished the ObjectSpawn script, it works fine

--- XRI_Project/Assets/Scripts/ObjectSpawner.cs
+++ XRI_Project/Assets/Scripts/ObjectSpawner.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.InputSystem.XR; // For communicating with quest controllers
 /*
 select to spawn
 where to object spawns
@@ -12,19 +14,52 @@ public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectPrefab; //Object to spawn
     public Transform spawnPoint; //Where it spawns
-    public XRNode controllerNode = XRNode.RightHand;
+    public XRNode controllerNode = XRNode.RightHand; //Assigning right hand controller
     public float spawnCooldown = 1.0f; //Need coroutine
-    private bool canSpawn = true;
+    private bool canSpawn = true; //Time in seconds between spawns
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canSpawn && IsAButtonPressed())
+        {
+            StartCoroutine(SpawnObjectWithCooldown());
+        }
+    }
+
+    bool IsAButtonPressed()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(controllerNode);
+        bool buttonPressed = false;
+
+        if (device.TryGetFeatureValue(CommonUsages.primaryButton, out buttonPressed) && buttonPressed) //Primary button is "a" or "x" on oculus controller
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator SpawnObjectWithCooldown()
+    {
+        canSpawn = false; //Prevents immediate respawning
+        SpawnObject();
+        yield return new WaitForSeconds(spawnCooldown);
+        canSpawn = true; //ALlows us to spawn again
+       
+    }
+
+    void SpawnObject()
+    {
+        if (objectPrefab != null && spawnPoint != null)
+        {
+            GameObject spawnedObject = Instantiate(objectPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        else
+        {
+            Debug.LogError("Assign objectPrefab and spawnPoint in the inspector.");
+        }
     }
 }
